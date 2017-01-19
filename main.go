5789c4f453e7480e0ac992cb816d8d1c84dce8e0
Justin Overfelt <joverfelt@gmail.com>
2017-01-18 19:32:13 -0500

    add final support for operations

--- main.go
+++ main.go
@@ -4,11 +4,46 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"plugin"
 	"regexp"
 )
 
 func main() {
-	fmt.Println(getPlugins())
+	pluginPaths := getPlugins()
+	var plugins []*plugin.Plugin
+	for _, path := range pluginPaths {
+		p, err := plugin.Open("plugins/" + path)
+		if err != nil {
+			panic(err)
+		}
+		plugins = append(plugins, p)
+	}
+
+	addFunc := findPlugin(plugins, "add")
+	subtractFunc := findPlugin(plugins, "subtract")
+
+	fmt.Println(addFunc(1, 1))
+	fmt.Println(subtractFunc(2, 1))
+}
+
+func findPlugin(plugins []*plugin.Plugin, name string) func(int, int) int {
+	for _, plugin := range plugins {
+		opSymbol, err := plugin.Lookup("OpName")
+		if err != nil {
+			panic(err)
+		}
+		op := opSymbol.(*string)
+
+		if *op == name {
+			funcSymbol, err := plugin.Lookup("Operation")
+			if err != nil {
+				panic(err)
+			}
+			f := funcSymbol.(func(int, int) int)
+			return f
+		}
+	}
+	return nil
 }
 
 func getPlugins() []string {
